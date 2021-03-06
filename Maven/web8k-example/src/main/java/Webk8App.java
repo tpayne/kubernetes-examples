3f5b19190de36ec01b417f93260842503354299f
Tim Payne <tim.tpayne@gmail.com>
2021-03-06 20:17:01 +0000

    Make case consistent

--- Maven/web8k-example/src/main/java/Webk8App.java
+++ Maven/web8k-example/src/main/java/Webk8App.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class webk8App {
+public class Webk8App {
     public static void main(String[] args) {
-        SpringApplication.run(webk8App.class, args);
+        SpringApplication.run(Webk8App.class, args);
     }
 }
