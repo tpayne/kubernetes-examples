d498915baf3b40e6ff367ccb900ec5b504393af7
Tim Payne <tim.tpayne@gmail.com>
2021-05-26 19:37:52 +0100

    Update build

--- gitops/gcp/create_cluster.sh
+++ gitops/gcp/create_cluster.sh
@@ -66,9 +66,15 @@ return 0
 
 create_cluster()
 {
-(gcloud container --project "${1}" clusters create "${2}" --zone "${3}" --no-enable-basic-auth --cluster-version "1.19.9-gke.1400" --release-channel "regular" --machine-type "n1-standard-2" --image-type "UBUNTU_CONTAINERD" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --max-pods-per-node "10" --preemptible --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/${1}/global/networks/default" --subnetwork "projects/${1}/regions/${4}/subnetworks/default" --no-enable-intra-node-visibility --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing,CloudRun,GcePersistentDiskCsiDriver --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes --tags "gitops" --node-locations "${3}") \
+(gcloud container --project "${1}" clusters create "${2}" --zone "${3}" --no-enable-basic-auth --cluster-version "1.19.9-gke.1400" --release-channel "regular" --machine-type "n1-standard-2" --image-type "UBUNTU_CONTAINERD" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --max-pods-per-node "20" --preemptible --num-nodes "6" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/${1}/global/networks/default" --subnetwork "projects/${1}/regions/${4}/subnetworks/default" --no-enable-intra-node-visibility --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing,CloudRun,GcePersistentDiskCsiDriver --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes --tags "gitops" --node-locations "${3}") \
     > ${tmpFile} 2>&1
 
+return $?
+}
+
+install_creds()
+{
+(gcloud container clusters get-credentials "${1}" --zone "${2}") > /dev/null 2>&1
 return $?
 }
 
@@ -87,6 +93,12 @@ if [ $? -gt 0 ]; then
    exit 1
 else
    echo "Cluster created"
+fi
+
+install_creds "${clusterId}" "${zone}"
+if [ $? -gt 0 ]; then
+   echo "Error: Cluster get-credentials failed"
+   exit 1
 fi
 
 exit 0
