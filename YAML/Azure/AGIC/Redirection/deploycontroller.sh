310e64c9c396dccd055a4347e4805320c1f09aee
Tim Payne <tim.tpayne@gmail.com>
2021-11-30 15:14:35 +0000

    Add more examples for AGIC

--- YAML/Azure/AGIC/Redirection/deploycontroller.sh
+++ YAML/Azure/AGIC/Redirection/deploycontroller.sh
@@ -7,8 +7,10 @@ trap 'stty echo; echo "${command} aborted"; exit' 1 2 3 15
 CWD=`pwd`
 
 group="aksagictest"
+name="${group}001"
 location="ukwest"
 remove=0
+getIp=0
 tmpFile="/tmp/tmpFile$$.tmp"
 
 rmFile()
@@ -33,6 +35,7 @@ while [ $# -ne 0 ] ; do
              -l | --location) location=$2
                  shift 2;;
              -d | --delete) remove=1 ; shift;;
+             --getIp) getIp=1 ; shift;;
              --debug) set -xv ; shift;;
              -?*) show_usage ; break;;
              --) shift ; break;;
@@ -74,9 +77,9 @@ fi
 exIpAddr="`dig +short myip.opendns.com @resolver1.opendns.com`"
 ipAddr="`ipconfig getifaddr en0`"
 echo "${command}: - create K8s system scoped to ${exIpAddr}..."
-(az aks create -n testaks001 -g $1 --network-plugin azure \
+(az aks create -n "${name}" -g $1 --network-plugin azure \
     --enable-managed-identity -a ingress-appgw \
-    --appgw-name myApplicationGateway --appgw-subnet-cidr "10.2.0.0/16" \
+    --appgw-name "${name}" --appgw-subnet-cidr "10.2.0.0/16" \
     --generate-ssh-keys \
     --api-server-authorized-ip-ranges "${exIpAddr}/32") > ${tmpFile} 2>&1
 if [ $? -gt 0 ]; then
@@ -85,8 +88,8 @@ if [ $? -gt 0 ]; then
     return 1
 fi
 
-echo "${command}: - enable K8s system..."
-(az aks get-credentials -n testaks001 -g $1 --overwrite-existing) > ${tmpFile} 2>&1
+echo "${command}: - enable K8s system ${name} ${group}..."
+(az aks get-credentials -n "${name}" -g $1 --overwrite-existing) > ${tmpFile} 2>&1
 if [ $? -gt 0 ]; then
     cat "${tmpFile}"
     rmFile "${tmpFile}"
@@ -96,7 +99,28 @@ fi
 return 0
 }
 
+getFrontEndIp()
+{
+echo "${command}: Getting gateway ip address for ${name} ${group}..."
+rmFile "${tmpFile}"
+appgw="`az aks show -g \"${1}\" -n \"${name}\" | grep effectiveApplicationGatewayId | gawk '{print $2}' | sed 's/.$//'`"
+
+# For some reason, this seems to be the only way to get the shell to protect the quotes correctly...
+echo "(az network application-gateway show --id \"${appgw}\" --query 'frontendIpConfigurations[].publicIpAddress.id' -o tsv)" | \
+    sh > ${tmpFile} 2>&1
+publicIpId="`cat ${tmpFile}`"
+xipAddr="`az network public-ip show --ids \"${publicIpId}\" --query "{ ipAddress: ipAddress }" --out tsv`"
+rmFile "${tmpFile}"
+echo "${command}: - Gateway IP address is \"${xipAddr}\""
+return 0
+}
+
 usage $*
+
+if [ $getIp -gt 0 ]; then
+    getFrontEndIp ${group}
+    exit 0
+fi
 
 cleanUp ${group} ${location}
 if [ $? -ne 0 -a $remove -gt 0 ]; then
@@ -113,5 +137,9 @@ if [ $? -ne 0 ]; then
     echo "${command}: - Error: The installation of the controller failed"
     exit 1
 fi
+
+# Sleep to give the resources time to spin up...
+sleep 180
+getFrontEndIp ${group}
 
 exit 0
