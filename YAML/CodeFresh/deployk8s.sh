b6eeb820d0cd370294e4e3769b2986995129a410
Tim Payne <tim.tpayne@gmail.com>
2022-03-04 11:46:57 +0000

    Add changes

--- YAML/CodeFresh/deployk8s.sh
+++ YAML/CodeFresh/deployk8s.sh
@@ -11,7 +11,7 @@ name="${group}001"
 location="ukwest"
 remove=0
 tmpFile="/tmp/tmpFile$$.tmp"
-
+authIps="107.21.238.215,18.209.185.91,18.215.207.215,18.233.130.31,18.210.174.176,23.20.5.235,3.232.154.67,34.192.31.53,34.193.111.98,34.195.17.245,34.196.33.69,34.198.38.4,34.200.163.76,44.238.236.43,44.234.209.117,44.239.141.205,44.228.66.171,44.238.167.159,44.237.63.217"
 rmFile()
 {
 if [ -f "$1" ]; then
@@ -82,7 +82,7 @@ echo "${command}: - create K8s system scoped to ${exIpAddr}..."
 (az aks create -n "${name}" -g $1 --network-plugin azure \
     --enable-managed-identity \
     --generate-ssh-keys \
-    --api-server-authorized-ip-ranges "${exIpAddr}/32") > ${tmpFile} 2>&1
+    --api-server-authorized-ip-ranges "${exIpAddr}/32","${authIps}") > ${tmpFile} 2>&1
 if [ $? -gt 0 ]; then
     cat "${tmpFile}"
     rmFile "${tmpFile}"
